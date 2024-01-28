50bb918e3dd503af9b2aad4c72dc93d8e3ba9c14
Soufiane Fathaoui <soufianenakata7@gmail.com>
2024-01-28 18:19:32 +0100

    the beginning

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainContent from './main_content';
+
+function App() {
+  return (
+    <div>
+      <MainContent/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main_content.jsx
@@ -0,0 +1,16 @@
+const MainContent = () => {
+  return (
+    <main className="flex border">
+      <div className="left_section">
+        <p>متبقي حتى صلاة</p>
+        <h1>10:10:10</h1>
+      </div>
+      <div className="right_section">
+        <p>متبقي حتى صلاة</p>
+        <h1>10:10:10</h1>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
