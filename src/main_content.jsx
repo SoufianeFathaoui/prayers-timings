b620edad8436071eae8d486085a9bb6de7267726
Soufiane Fathaoui <soufianenakata7@gmail.com>
2024-01-29 14:25:19 +0100

    Addin the first step

--- src/main_content.jsx
+++ src/main_content.jsx
@@ -1,13 +1,15 @@
+import './main_content.css';
 const MainContent = () => {
   return (
-    <main className="flex border">
-      <div className="left_section">
+    <main className="flex continer">
+      <div></div>
+      <div className="left_section flex ">
         <p>متبقي حتى صلاة</p>
         <h1>10:10:10</h1>
       </div>
-      <div className="right_section">
+      <div className="right_section flex">
         <p>متبقي حتى صلاة</p>
-        <h1>10:10:10</h1>
+        <h1>مكة المكرمة</h1>
       </div>
     </main>
   );
